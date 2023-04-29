a160582e4702421f43b02dfa94954ee085248f76
Marwan Rahim <marwan820@Marwan>
2023-04-28 20:44:47 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution here
+
+function findMatching(array,name){
+  const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+  return array.filter(driver => driver.toLowerCase() === name.toLowerCase())
+  }
+  
+  function fuzzyMatch(array,name){
+    const  drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+    return array.filter(driver => driver.toLowerCase().startsWith(name.toLowerCase()));
+  }
+  
+  const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }]
+
+function matchName(array,name){
+
+  
+return array.filter(driver => driver.name === name)
+}
+        
+  
